d9b4250cc9f454ff6ee4c641e5202f291fe20dad
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-11-24 17:57:09 +0300

    tast 1 - ok

--- jest/test/unit/sortByName.test.js
+++ jest/test/unit/sortByName.test.js
@@ -1,17 +1,40 @@
 const sorting = require('../../app');
 
+const config = {
+    collectCoverageFrom: [
+      '**/*.{js,jsx}',
+      '!**/node_modules/**',
+      '!**/coverage/**',
+    ],
+};
+  
+module.exports = config;  
+
 describe('Books names test suit', () => {
-    it('Books names should be sorted in ascending order', () => {
-        expect(
-            sorting.sortByName([
-                'Гарри Поттер',
-                'Властелин Колец',
-                'Волшебник изумрудного города',
-            ]),
-        ).toEqual([
+    test('Books names should be sorted in ascending order', () => {
+        const expected = sorting.sortByName([
+            'Гарри Поттер',
+            'Властелин Колец',
+            'Волшебник изумрудного города',
+        ]);
+        const actual = [
             'Властелин Колец',
             'Волшебник изумрудного города',
             'Гарри Поттер',
+        ];
+        expect(expected).toEqual(actual);
+    });
+    test('Books names should be sorted in ascending order', () => {
+        const expected = sorting.sortByName([
+            'Гарри Поттер',
+            'Гарри Поттер',
+            'Волшебник изумрудного города',
         ]);
+        const actual = [
+            'Волшебник изумрудного города',
+            'Гарри Поттер',
+            'Гарри Поттер',
+        ];
+        expect(expected).toEqual(actual);
     });
 });
